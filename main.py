e1e6ceea31b71069e455dbf1ee19c4d2e5389794
Gligor Milicevic <gligormili@gmail.com>
2026-04-16 14:33:56 +0200

    Manual change on main branch to create conflict

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from product import Product
 from product_manager import ProductManager
 
 manager = ProductManager()
-p1 = Product("Laptop", 1200, 5)
+# p1 = Product("Laptop", 1200, 5)
+p1 = Product("Super Laptop", 1200, 5)
 p2 = Product("Mouse", 25, 20)
 
 manager.add_product(p1)
@@ -11,3 +12,6 @@ manager.add_product(p2)
 print("Inventory List:")
 manager.display_all_products()
 print(f"Total Value: {manager.total_inventory_value()}")
+
+
+# Edit main.py manually to change product names/quantities and delete display lines.
